f3cefc90bc92ac897a391dbbc1abc90b0539422d
ltsyyds <107777288+ltsyydswow@users.noreply.github.com>
2023-07-13 11:12:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://5244-c8317a7d4ef949649fc64e78a1b39b9b.onpatr.cloud/'
+    if (url.pathname.startsWith('https://chat.acytoo.com/')) {
+      url.hostname = 'https'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
